f36ea8c2ee5304bde6135a4d116b786310c8722f
zsofi21 <szentpeteri.zsofia.21@gmail.com>
2023-09-23 17:35:42 +0200

    hibák kommentelve

--- main.cpp
+++ main.cpp
@@ -4,23 +4,23 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS]; //typo
+    std::cout << '1-100 ertekek duplazasa' //;
+    for (int i = 0;) //primary expression hianyzik
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++) //hibas primary expression
     {
-        std::cout << "Ertek:"
-    }    
+        std::cout << "Ertek:" //nem irja ki az erteket
+    }
     std::cout << "Atlag szamitasa: " << std::endl;
     int atlag;
     for (int i = 0; i < N_ELEMENTS, i++)
     {
-        atlag += b[i]
+        atlag += b[i] //;
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
-    return 0;
+    return 0; //memoria nem kerul felszabaditasra
 }
